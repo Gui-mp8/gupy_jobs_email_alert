da898716f57a230f2c15c8097c2d89931492153b
guilherme machado <guilherme.machado@indicium.tech>
2023-07-19 11:47:00 -0300

    adding main executable file

---
+++ gupy-scraper/main.py
@@ -0,0 +1,9 @@
+from utils.config import load_config
+from scraper.web_scraper import WebScraper
+
+def main(config):
+    return print(WebScraper(config).extract_data_to_json())
+
+if __name__ == "__main__":
+    config = load_config()
+    main(config)
