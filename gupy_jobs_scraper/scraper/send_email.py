d0125bb47fdc3360f41bcd3945cebcaba0704fc3
Guilherme Machado <guilherme.machado@indicium.tech>
2023-07-19 18:42:13 -0300

    .

--- gupy_jobs_scraper/scraper/send_email.py
+++ gupy_jobs_scraper/scraper/send_email.py
@@ -4,15 +4,17 @@ import smtplib
 import ssl
 from email.message import EmailMessage
 from datetime import date
+from typing import Dict, List, Any
+
 
 class SendEmail():
-    def __init__(self, config) -> None:
+    def __init__(self, config: Dict[str, Any]) -> None:
         self.user_email = config["user_email"]
         self.password = config["password"]
         self.receiver_email = config["receiver_email"]
         self.message = EmailMessage()
 
-    def create_email(self, json_data):
+    def create_email(self, json_data: List[Dict[str, Any]]) -> EmailMessage:
         today = date.today().strftime('%Y-%m-%d')
 
         self.message['From'] = self.user_email
@@ -50,8 +52,7 @@ class SendEmail():
         context = ssl.create_default_context()
         return context
 
-
-    def send_email(self, json_data):
+    def send_email(self, json_data: List[Dict[str, Any]]):
         with smtplib.SMTP_SSL('smtp.gmail.com', 465, context=self.create_email(json_data)) as smtp:
             smtp.login(self.user_email, self.password)
             smtp.sendmail(self.user_email, self.receiver_email, self.message.as_string())
