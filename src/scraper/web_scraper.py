6b42c42c359c224932f9aa2e4012b69b92bcac1e
Guilherme Machado <guilherme.machado@indicium.tech>
2023-07-24 19:28:26 -0300

    multiple researchs

--- src/scraper/web_scraper.py
+++ src/scraper/web_scraper.py
@@ -2,28 +2,33 @@ from bs4 import BeautifulSoup
 import requests
 import re
 from typing import Dict, List, Any
+import logging
 
 class WebScraper:
     def __init__(self, config: Dict[str, Any]) -> None:
-        self.url = 'https://portal.gupy.io/job-search/term=' + config['research']
-
-    def soup(self) -> BeautifulSoup:
-        response = requests.get(url=self.url)
-        return BeautifulSoup(response.content, 'html.parser')
+        self.base_url = 'https://portal.gupy.io/job-search/term='
+        self.config = config
+        self.data = []
 
     def extract_data_to_json(self) -> List[Dict[str, Any]]:
-        job_blocks = self.soup().find_all('div', class_="sc-70b75bd4-0 hojCpd")
-        data_list = [
-            {
-                'company_name': job_block.find('p', class_='sc-efBctP dpAAMR sc-70b75bd4-5 dCVCel').get_text(),
-                'job_name': job_block.find('h4', class_='sc-llJcti bGqDEZ sc-70b75bd4-7 ftHylk').get_text(),
-                'job_location': job_block.find('p', class_='sc-efBctP dpAAMR sc-70b75bd4-4 sc-70b75bd4-6 cpauqG jJvUPa').get_text(),
-                'job_type': job_block.find('p', class_='sc-efBctP dpAAMR sc-70b75bd4-4 cpauqG').get_text(),
-                'job_date_creation': job_block.find(text=re.compile(r'\d{2}/\d{2}/\d{4}')),
-                'job_link': job_block.find('a')['href']
-            }
-            for job_block in job_blocks
-        ]
+        for research in self.config['research']:
+            url = self.base_url + research
+            response = requests.get(url=url)
+            soup = BeautifulSoup(response.content, 'html.parser')
 
-        return data_list
+            job_blocks = soup.find_all('div', class_="sc-70b75bd4-0 hojCpd")
+            data_list = [
+                {
+                    'company_name': job_block.find('p', class_='sc-efBctP dpAAMR sc-70b75bd4-5 dCVCel').get_text(),
+                    'job_name': job_block.find('h4', class_='sc-llJcti bGqDEZ sc-70b75bd4-7 ftHylk').get_text(),
+                    'job_location': job_block.find('p', class_='sc-efBctP dpAAMR sc-70b75bd4-4 sc-70b75bd4-6 cpauqG jJvUPa').get_text(),
+                    'job_type': job_block.find('p', class_='sc-efBctP dpAAMR sc-70b75bd4-4 cpauqG').get_text(),
+                    'job_date_creation': job_block.find(text=re.compile(r'\d{2}/\d{2}/\d{4}')),
+                    'job_link': job_block.find('a')['href']
+                }
+                for job_block in job_blocks
+            ]
+
+            self.data.extend(data_list)
+        return self.data
 
