4fe3babab2ab1bd7185f425199cfd045df6488f4
Guilherme Machado <guilherme.machado@indicium.tech>
2023-07-24 19:28:45 -0300

    logging

--- src/scraper/__init__.py
+++ src/scraper/__init__.py
@@ -1,2 +1,12 @@
-print(f'Invoking __init__.py for {__name__}')
-import scraper.web_scraper
+import logging
+
+# Configure the logging settings
+logging.basicConfig(
+    level=logging.INFO,  # Set the desired log level (e.g., DEBUG, INFO, WARNING, ERROR, CRITICAL)
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+)
+
+# Create a logger object
+logger = logging.getLogger(__name__)
+
+logging.info(f'STARTING: {__name__} aplication')
