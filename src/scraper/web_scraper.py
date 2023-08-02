ece2b833f08920ddf392ac7c3f9b4089c7672eb2
Guilherme Machado <guilherme.machado@indicium.tech>
2023-08-02 00:32:58 -0300

    updated because of the site code change

--- src/scraper/web_scraper.py
+++ src/scraper/web_scraper.py
@@ -16,14 +16,14 @@ class WebScraper:
             response = requests.get(url=url)
             soup = BeautifulSoup(response.content, 'html.parser')
 
-            job_blocks = soup.find_all('div', class_="sc-70b75bd4-0 hojCpd")
+            job_blocks = soup.find_all('div', class_="sc-f31b108-0 bChLuL")
             data_list = [
                 {
-                    'company_name': job_block.find('p', class_='sc-efBctP dpAAMR sc-70b75bd4-5 dCVCel').get_text(),
-                    'job_name': job_block.find('h4', class_='sc-llJcti bGqDEZ sc-70b75bd4-7 ftHylk').get_text(),
-                    'job_location': job_block.find('p', class_='sc-efBctP dpAAMR sc-70b75bd4-4 sc-70b75bd4-6 cpauqG jJvUPa').get_text(),
-                    'job_type': job_block.find('p', class_='sc-efBctP dpAAMR sc-70b75bd4-4 cpauqG').get_text(),
-                    'job_date_creation': job_block.find(text=re.compile(r'\d{2}/\d{2}/\d{4}')),
+                    'company_name': job_block.find('p', class_='sc-efBctP dpAAMR sc-f31b108-6 jguNee').get_text(),
+                    'job_name': job_block.find('h2', class_='sc-llJcti jgKUZ sc-f31b108-5 dILyqK').get_text(),
+                    'job_location': job_block.find('span', class_='sc-23336bc7-1 cezNaf').get_text(),
+                    'job_type': [job_type.get_text() for job_type in job_block.find_all('span', class_='sc-23336bc7-1 cezNaf')[1:]],
+                    'job_date_creation': job_block.find('p', class_='sc-efBctP dpAAMR sc-1db88588-0 inqtnx').get_text().split(': ')[1],
                     'job_link': job_block.find('a')['href']
                 }
                 for job_block in job_blocks
@@ -31,4 +31,3 @@ class WebScraper:
 
             self.data.extend(data_list)
         return self.data
-
