feed9bb9e471b748b4de52981bd49c1d92d71228
Guilherme Machado <guilherme.machado@indicium.tech>
2023-09-16 20:14:13 -0300

    Creating a new module to validate the data before arriving to the final destination.

---
+++ src/validation/validation.py
@@ -0,0 +1,30 @@
+from typing import Dict, List, Any
+from pydantic import BaseModel, ValidationError
+from utils.logger import log
+
+class ValidatedData(BaseModel):
+    company_name: str
+    job_name: str
+    job_location: str
+    job_type: str
+    job_date_creation: str
+    job_link: str
+
+class DataValidator:
+    @staticmethod
+    def result_validation(list_dict_data: List[Dict[str, Any]]) -> List[Dict[str, Any]]:
+        validated_data = []
+
+        for item in list_dict_data:
+            try:
+                ValidatedData(**item)  # Try to create an instance of ValidatedData with the dictionary
+                validated_data.append(item)  # If successful, add it to the validated data list
+            except ValidationError as e:
+                log().error(f"Validation error: {e}")
+
+        if not validated_data:
+            log().info("No valid job data found")
+        else:
+            log().info("Validation Completed!")
+
+        return validated_data
