7a9d101371d096a31601b9f80cdeec295f2c4418
Guilherme Machado <guilherme.machado@indicium.tech>
2023-11-11 00:05:48 -0300

    feat: adding async extraction

--- src/main.py
+++ src/main.py
@@ -1,18 +1,25 @@
+import time
+from typing import Dict, List, Any
+
+import asyncio
+
 from utils.config import load_config
 from scraper.web_scraper import WebScraper
 from transformation.transformation import Transform
 from validation.validation import DataValidator
 from send_email.send_email import SendEmail
 
-from typing import Dict, List, Any
-
-def main(config: Dict[str,Any]) -> List[Dict[str,Any]]:
-    data = WebScraper(config).extract_data_to_json()
+def main(config: Dict[str,Any]):
+    data = asyncio.run(WebScraper(config).extract_data_to_json())
     list_dict_data = Transform(config, json_data=data).data_filter()
     validated_data = DataValidator.result_validation(list_dict_data=list_dict_data)
 
     return SendEmail(config, validated_data).send_email()
 
 if __name__ == "__main__":
+    start_time = time.time()
     config = load_config()
     main(config)
+    end_time = time.time()
+    elapsed_time = end_time - start_time
+    print(f"Program execution time: {elapsed_time} seconds")
